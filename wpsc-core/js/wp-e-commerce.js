wp-e-commerce/WP-e-Commerce

commit bd62022b6f81e96e0f374f9a86a2daae443cf9aa

Find action as sibling of submit button (child of current form)

wpsc-core/js/wp-e-commerce.js
@@ -239,7 +239,9 @@ jQuery(document).ready(function ($) {
 		if(file_upload_elements.length > 0) {
 			return true;
 		} else {
-			form_values = jQuery(this).serialize() + '&action=' + jQuery( 'input[name="wpsc_ajax_action"]' ).val();
+			var action_buttons = jQuery(this).children('input[name="wpsc_ajax_action"]');
+			var action = action_buttons[0].value;
+			form_values = jQuery(this).serialize() + '&action=' + action;
 
 			// Sometimes jQuery returns an object instead of null, using length tells us how many elements are in the object, which is more reliable than comparing the object to null
 			if( jQuery( '#fancy_notification' ).length === 0 ) {

konstiantynr/wordpress-ecommerce

commit 8b0d0d0ff3aca8f1688a660a873f4ab4e1a528c9

There was an issue caused by bd62022b6f81e96e0f374f9a86a2daae443cf9aa that resulted in Add to Cart Fancy notifications failing in some contexts, like hsortcodes.

wpsc-core/js/wp-e-commerce.js
@@ -311,7 +311,7 @@ jQuery(document).ready(function ($) {
 		if(file_upload_elements.length > 0) {
 			return true;
 		} else {
-			var action_buttons = jQuery(this).children('input[name="wpsc_ajax_action"]');
+			var action_buttons = jQuery( 'input[name="wpsc_ajax_action"]', jQuery( this ) );
 			var action = action_buttons[0].value;
 			form_values = jQuery(this).serialize() + '&action=' + action;
 