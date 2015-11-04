wp-e-commerce/WP-e-Commerce

commit d9864da668ad19302a4f86fdc8dbe693b299a4e8

Final pass at i18n fixes.

wpsc-admin/includes/save-data.functions.php
@@ -206,10 +206,10 @@ function wpsc_admin_category_forms_add() {
 					<?php foreach( $countrylist as $country ): ?>
 						<?php if ( in_array( $country['id'], (array)$selectedCountries ) ): ?>
 							<input type='checkbox' name='countrylist2[]' id='countrylist2-<?php echo $country['id']; ?>' value='<?php echo $country['id']; ?>' checked='<?php echo $country['visible']; ?>' />
-							<label for="countrylist2-<?php echo $country['id']; ?>"><?php esc_html_e( $country['country'] ); ?></label><br />
+							<label for="countrylist2-<?php echo $country['id']; ?>"><?php esc_html( $country['country'] ); ?></label><br />
 						<?php else: ?>
 							<input type='checkbox' name='countrylist2[]' id='countrylist2-<?php echo $country['id']; ?>' value='<?php echo $country['id']; ?>'  />
-							<label for="countrylist2-<?php echo $country['id']; ?>"><?php esc_html_e( $country['country'] ); ?></label><br />
+							<label for="countrylist2-<?php echo $country['id']; ?>"><?php esc_html( $country['country'] ); ?></label><br />
 						<?php endif; ?>
 					<?php endforeach; ?>
 				</div>
@@ -401,12 +401,12 @@ function wpsc_admin_category_forms_edit() {
 						if ( in_array( $country['id'], (array)$selectedCountries ) ) {
 							?>
 							<input type='checkbox' name='countrylist2[]' id='countrylist2-<?php echo $country['id']; ?>' value='<?php echo $country['id']; ?>' checked='<?php echo $country['visible']; ?>' />
-							<label for="countrylist2-<?php echo $country['id']; ?>"><?php esc_html_e( $country['country'] ); ?></label><br />
+							<label for="countrylist2-<?php echo $country['id']; ?>"><?php esc_html( $country['country'] ); ?></label><br />
 							<?php
 						} else {
 							?>
 							<input type='checkbox' name='countrylist2[]' id='countrylist2-<?php echo $country['id']; ?>' value='<?php echo $country['id']; ?>'  />
-							<label for="countrylist2-<?php echo $country['id']; ?>"><?php esc_html_e( $country['country'] ); ?></label><br />
+							<label for="countrylist2-<?php echo $country['id']; ?>"><?php esc_html( $country['country'] ); ?></label><br />
 							<?php
 						}
 					} ?>

wpsc-admin/includes/settings-tabs/general.php
@@ -95,10 +95,10 @@ class WPSC_Settings_Tab_General extends WPSC_Settings_Tab {
 								<?php foreach ( (array)$countrylist as $country ) : ?>
 									<?php if ( $country['visible'] == 1 ) : ?>
 										<input type='checkbox' id="countrylist2-<?php echo $country['id']; ?>" name='countrylist2[]' value='<?php echo $country['id']; ?>' checked='checked' />
-										<label for="countrylist2-<?php echo $country['id']; ?>"><?php esc_html_e( $country['country'] ); ?></label><br />
+										<label for="countrylist2-<?php echo $country['id']; ?>"><?php esc_html( $country['country'] ); ?></label><br />
 									<?php else : ?>
 										<input type='checkbox' id="countrylist2-<?php echo $country['id']; ?>" name='countrylist2[]' value='<?php echo $country['id']; ?>'  />
-										<label for="countrylist2-<?php echo $country['id']; ?>"><?php esc_html_e( $country['country'] ); ?></label><br />
+										<label for="countrylist2-<?php echo $country['id']; ?>"><?php esc_html( $country['country'] ); ?></label><br />
 									<?php endif; ?>
 								<?php endforeach; ?>
 							</div>
@@ -155,7 +155,7 @@ class WPSC_Settings_Tab_General extends WPSC_Settings_Tab {
 				<td>
 					<select id="wpsc_options_currency_type" name='wpsc_options[currency_type]' onchange='getcurrency(this.options[this.selectedIndex].value);'>
 					<?php foreach ( $currency_data as $currency ) : ?>
-						<option value='<?php echo $currency['id']; ?>' <?php selected( $currency['id'], $currency_type ); ?>><?php esc_html_e( $currency['country'] ); ?> (<?php echo $currency['currency']; ?>)</option>
+						<option value='<?php echo $currency['id']; ?>' <?php selected( $currency['id'], $currency_type ); ?>><?php esc_html( $currency['country'] ); ?> (<?php echo $currency['currency']; ?>)</option>
 					<?php endforeach; ?>
 					</select>
 				</td>

wpsc-components/theme-engine-v2/helpers/template-tags/general.php
@@ -432,13 +432,13 @@ function wpsc_get_customer_orders_statuses() {
 	$controller = _wpsc_get_current_controller();
 
 	$view_labels = array(
-		0 => _nx_noop( 'All <span class="count">(%s)</span>'       , 'All <span class="count">(%s)</span>'       , 'purchase logs' ),
-		1 => _nx_noop( 'Incomplete <span class="count">(%s)</span>', 'Incomplete <span class="count">(%s)</span>', 'purchase logs' ),
-		2 => _nx_noop( 'Received <span class="count">(%s)</span>'  , 'Received <span class="count">(%s)</span>'  , 'purchase logs' ),
-		3 => _nx_noop( 'Accepted <span class="count">(%s)</span>'  , 'Accepted <span class="count">(%s)</span>'  , 'purchase logs' ),
-		4 => _nx_noop( 'Dispatched <span class="count">(%s)</span>', 'Dispatched <span class="count">(%s)</span>', 'purchase logs' ),
-		5 => _nx_noop( 'Closed <span class="count">(%s)</span>'    , 'Closed <span class="count">(%s)</span>'    , 'purchase logs' ),
-		6 => _nx_noop( 'Declined <span class="count">(%s)</span>'  , 'Declined <span class="count">(%s)</span>'  , 'purchase logs' ),
+		0 => _nx_noop( 'All <span class="count">(%s)</span>'       , 'All <span class="count">(%s)</span>'       , 'purchase logs', 'wp-e-commerce' ),
+		1 => _nx_noop( 'Incomplete <span class="count">(%s)</span>', 'Incomplete <span class="count">(%s)</span>', 'purchase logs', 'wp-e-commerce' ),
+		2 => _nx_noop( 'Received <span class="count">(%s)</span>'  , 'Received <span class="count">(%s)</span>'  , 'purchase logs', 'wp-e-commerce' ),
+		3 => _nx_noop( 'Accepted <span class="count">(%s)</span>'  , 'Accepted <span class="count">(%s)</span>'  , 'purchase logs', 'wp-e-commerce' ),
+		4 => _nx_noop( 'Dispatched <span class="count">(%s)</span>', 'Dispatched <span class="count">(%s)</span>', 'purchase logs', 'wp-e-commerce' ),
+		5 => _nx_noop( 'Closed <span class="count">(%s)</span>'    , 'Closed <span class="count">(%s)</span>'    , 'purchase logs', 'wp-e-commerce' ),
+		6 => _nx_noop( 'Declined <span class="count">(%s)</span>'  , 'Declined <span class="count">(%s)</span>'  , 'purchase logs', 'wp-e-commerce' ),
 	);
 
 	$views = array();

wpsc-admin/db-upgrades/upgrade.php
@@ -55,7 +55,7 @@ function _wpsc_upgrade_display_backup_warning() {
 }
 
 function _wpsc_upgrade_display_prompt() {
-	$message = __( '<strong>WP eCommerce %1$s is almost ready.</strong> Some database routines need to be run before the upgrade is complete. <a href="%2$s">Click here to start!</a>', 'wpsc' );
+	$message = __( '<strong>WP eCommerce %1$s is almost ready.</strong> Some database routines need to be run before the upgrade is complete. <a href="%2$s">Click here to start!</a>', 'wp-e-commerce' );
 	$message = sprintf( $message, WPSC_VERSION, admin_url( '?page=wpsc-db-upgrade' ) );
 	?>
 	<div id="wpsc-upgrade-warning" class="error">

wpsc-admin/display-sales-logs.php
@@ -147,7 +147,7 @@ class WPSC_Purchase_Log_Page {
 				$disabled = '';
 			}
 			?>
-			<a href='<?php esc_attr_e( $href ); ?>' class='prev-page <?php echo $disabled; ?>'>&lsaquo; <?php _e( 'Previous', 'wp-e-commerce' ); ?></a>
+			<a href='<?php esc_url( $href ); ?>' class='prev-page <?php echo $disabled; ?>'>&lsaquo; <?php _e( 'Previous', 'wp-e-commerce' ); ?></a>
 			<?php
 
 			$max_purchase_id = wpsc_max_purchase_id();

wpsc-admin/includes/display-items-functions.php
@@ -147,7 +147,7 @@ function wpsc_price_control_forms() {
 ?>
 		<em id="wpsc_product_price_metabox_live_title" class="wpsc_metabox_live_title">
 			<p>&nbsp;<?php echo esc_html( $ct_symb ); ?><span><?php echo esc_html( $sale_price ); ?></span></p>
-			<del><?php esc_html_e($ct_symb); ?><span><?php esc_html_e($price) ?></span></del>
+			<del><?php esc_html( $ct_symb ); ?><span><?php esc_html( $price ) ?></span></del>
 		</em>
 		<input type="hidden" id="parent_post" name="parent_post" value="<?php echo $post->post_parent; ?>" />
 		<?php /* Lots of tedious work is avoided with this little line. */ ?>
@@ -1147,15 +1147,15 @@ function wpsc_attachment_fields( $form_fields, $post ) {
 
 		jQuery("a.wp-post-thumbnail").each(function(){
 			var product_image = jQuery(this).text();
-			if (product_image == "' . esc_js( __( 'Use as featured image' ) ) . '") {
-				jQuery(this).text("' . esc_js( __('Use as Product Thumbnail', 'wp-e-commerce') ) . '");
+			if (product_image == "' . esc_js( __( 'Use as featured image', 'wp-e-commerce' ) ) . '") {
+				jQuery(this).text("' . esc_js( __('Use as Product Thumbnail', 'wp-e-commerce' ) ) . '");
 			}
 		});
 
 		var trash = jQuery("#media-upload a.del-link").text();
 
-		if (trash == "' . esc_js( __( 'Delete' ) ) . '") {
-			jQuery("#media-upload a.del-link").text("' . esc_js( __( 'Trash' ) ) . '");
+		if (trash == "' . esc_js( __( 'Delete', 'wp-e-commerce' ) ) . '") {
+			jQuery("#media-upload a.del-link").text("' . esc_js( __( 'Trash', 'wp-e-commerce' ) ) . '");
 		}
 
 

wpsc-admin/includes/product-variation-list-table.class.php
@@ -129,15 +129,16 @@ class WPSC_Product_Variation_List_Table extends WP_List_Table {
 	public function get_columns() {
 		$columns = array(
 			'cb'         => '<input type="checkbox" />',
-			'title'      => __( 'Title', 'wp-e-commerce' ),
-			'sku'        => __( 'SKU', 'wp-e-commerce' ),
-			'price'      => __( 'Price', 'wp-e-commerce' ),
+			'title'      => __( 'Title'     , 'wp-e-commerce' ),
+			'sku'        => __( 'SKU'       , 'wp-e-commerce' ),
+			'price'      => __( 'Price'     , 'wp-e-commerce' ),
 			'sale_price' => __( 'Sale Price', 'wp-e-commerce' ),
-			'stock'      => __( 'Stock', 'wp-e-commerce' ),
+			'stock'      => __( 'Stock'     , 'wp-e-commerce' ),
 		);
 
-		if ( get_option( 'wpec_taxes_enabled' ) )
+		if ( get_option( 'wpec_taxes_enabled' ) ) {
 			$columns['tax'] = __( 'Taxable Amount', 'wp-e-commerce' );
+		}
 
 		return apply_filters( 'wpsc_variation_column_headers', $columns );
 	}
@@ -158,11 +159,13 @@ class WPSC_Product_Variation_List_Table extends WP_List_Table {
 
 	private function get_row_actions( $item ) {
 		$post_type_object = get_post_type_object( 'wpsc-product' );
-		$can_edit_post = current_user_can( $post_type_object->cap->edit_post, $item->ID );
+		$can_edit_post    = current_user_can( $post_type_object->cap->edit_post, $item->ID );
 
 		$actions = array();
-		if ( apply_filters( 'wpsc_show_product_variations_edit_action', true, $item ) && $can_edit_post && 'trash' != $item->post_status )
-			$actions['edit'] = '<a target="_blank" href="' . get_edit_post_link( $item->ID, true ) . '" title="' . esc_attr( __( 'Edit this item' ), 'wp-e-commerce' ) . '">' . __( 'Edit' ) . '</a>';
+
+		if ( apply_filters( 'wpsc_show_product_variations_edit_action', true, $item ) && $can_edit_post && 'trash' != $item->post_status ) {
+			$actions['edit'] = '<a target="_blank" href="' . get_edit_post_link( $item->ID, true ) . '" title="' . esc_attr__( 'Edit this item', 'wp-e-commerce' ) . '">' . __( 'Edit', 'wp-e-commerce' ) . '</a>';
+		}
 
 		$actions['stock hide-if-no-js'] = '<a class="wpsc-variation-stock-editor-link" href="#" title="' . __( 'Show shipping editor', 'wp-e-commerce' ) . '">' . __( 'Edit Shipping', 'wp-e-commerce' ) . '</a>';
 
@@ -197,13 +200,13 @@ class WPSC_Product_Variation_List_Table extends WP_List_Table {
 					$restore_url
 				);
 				$restore_url = wp_nonce_url( $restore_url, 'untrash-post_' . $item->ID );
-				$actions['untrash'] = "<a title='" . esc_attr( __( 'Restore this item from the Trash' ) ) . "' href='" . esc_url( $restore_url ) . "'>" . __( 'Restore' ) . "</a>";
+				$actions['untrash'] = "<a title='" . esc_attr__( 'Restore this item from the Trash', 'wp-e-commerce' ) . "' href='" . esc_url( $restore_url ) . "'>" . __( 'Restore', 'wp-e-commerce' ) . "</a>";
 			} elseif ( EMPTY_TRASH_DAYS ) {
-				$actions['trash'] = "<a class='submitdelete' title='" . esc_attr( __( 'Move this item to the Trash' ) ) . "' href='" . esc_url( $delete_link ) . "'>" . __( 'Trash' ) . "</a>";
+				$actions['trash'] = "<a class='submitdelete' title='" . esc_attr__( 'Move this item to the Trash', 'wp-e-commerce' ) . "' href='" . esc_url( $delete_link ) . "'>" . __( 'Trash', 'wp-e-commerce' ) . "</a>";
 			}
 
 			if ( $force_delete )
-				$actions['delete'] = "<a class='submitdelete' title='" . esc_attr( __( 'Delete this item permanently' ) ) . "' href='" . esc_url( $delete_link ) . "'>" . __( 'Delete Permanently' ) . "</a>";
+				$actions['delete'] = "<a class='submitdelete' title='" . esc_attr__( 'Delete this item permanently', 'wp-e-commerce' ) . "' href='" . esc_url( $delete_link ) . "'>" . __( 'Delete Permanently', 'wp-e-commerce' ) . "</a>";
 		}
 
 		return $actions;
@@ -239,7 +242,7 @@ class WPSC_Product_Variation_List_Table extends WP_List_Table {
 			<div class="wpsc-product-variation-title">
 				<strong class="row-title">
 					<?php if ( $show_edit_link ): ?>
-						<a target="_blank" href="<?php echo esc_url( get_edit_post_link( $item->ID, true ) ); ?>" title="<?php esc_attr_e( __( 'Edit this item' ), 'wp-e-commerce' ); ?>">
+						<a target="_blank" href="<?php echo esc_url( get_edit_post_link( $item->ID, true ) ); ?>" title="<?php esc_attr_e( 'Edit this item', 'wp-e-commerce' ); ?>">
 					<?php endif; ?>
 					<?php echo esc_html( apply_filters( 'wpsc_variation_name', $title, $item ) ); ?>
 					<?php if ( $show_edit_link ): ?>
@@ -385,7 +388,7 @@ class WPSC_Product_Variation_List_Table extends WP_List_Table {
 			return;
 
 		if ( isset($_REQUEST['updated']) && (int) $_REQUEST['updated'] ) {
-			$messages[] = sprintf( _n( '%s post updated.', '%s posts updated.', $_REQUEST['updated'] ), number_format_i18n( $_REQUEST['updated'] ) );
+			$messages[] = sprintf( _n( '%s post updated.', '%s posts updated.', $_REQUEST['updated'], 'wp-e-commerce' ), number_format_i18n( $_REQUEST['updated'] ) );
 			unset($_REQUEST['updated']);
 		}
 
@@ -393,25 +396,25 @@ class WPSC_Product_Variation_List_Table extends WP_List_Table {
 			unset($_REQUEST['skipped']);
 
 		if ( isset($_REQUEST['locked']) && (int) $_REQUEST['locked'] ) {
-			$messages[] = sprintf( _n( '%s item not updated, somebody is editing it.', '%s items not updated, somebody is editing them.', $_REQUEST['locked'] ), number_format_i18n( $_REQUEST['locked'] ) );
+			$messages[] = sprintf( _n( '%s item not updated, somebody is editing it.', '%s items not updated, somebody is editing them.', $_REQUEST['locked'], 'wp-e-commerce' ), number_format_i18n( $_REQUEST['locked'] ) );
 			unset($_REQUEST['locked']);
 		}
 
 		if ( isset($_REQUEST['deleted']) && (int) $_REQUEST['deleted'] ) {
-			$messages[] = sprintf( _n( 'Item permanently deleted.', '%s items permanently deleted.', $_REQUEST['deleted'] ), number_format_i18n( $_REQUEST['deleted'] ) );
+			$messages[] = sprintf( _n( 'Item permanently deleted.', '%s items permanently deleted.', $_REQUEST['deleted'], 'wp-e-commerce' ), number_format_i18n( $_REQUEST['deleted'] ) );
 			unset($_REQUEST['deleted']);
 		}
 
 		if ( isset($_REQUEST['trashed']) && (int) $_REQUEST['trashed'] ) {
-			$messages[] = sprintf( _n( 'Item moved to the Trash.', '%s items moved to the Trash.', $_REQUEST['trashed'] ), number_format_i18n( $_REQUEST['trashed'] ) );
+			$messages[] = sprintf( _n( 'Item moved to the Trash.', '%s items moved to the Trash.', $_REQUEST['trashed'], 'wp-e-commerce' ), number_format_i18n( $_REQUEST['trashed'] ) );
 			$ids = isset($_REQUEST['ids']) ? $_REQUEST['ids'] : 0;
 			$undo_url = wp_nonce_url( add_query_arg( array( 'doaction' => 'undo', 'action' => 'untrash', 'ids' => $ids ) ), 'bulk-posts' );
-			$messages[] = '<a href="' . esc_url( $undo_url ) . '">' . __('Undo') . '</a>';
+			$messages[] = '<a href="' . esc_url( $undo_url ) . '">' . __( 'Undo', 'wp-e-commerce' ) . '</a>';
 			unset($_REQUEST['trashed']);
 		}
 
 		if ( isset($_REQUEST['untrashed']) && (int) $_REQUEST['untrashed'] ) {
-			$messages[] = sprintf( _n( 'Item restored from the Trash.', '%s items restored from the Trash.', $_REQUEST['untrashed'] ), number_format_i18n( $_REQUEST['untrashed'] ) );
+			$messages[] = sprintf( _n( 'Item restored from the Trash.', '%s items restored from the Trash.', $_REQUEST['untrashed'], 'wp-e-commerce' ), number_format_i18n( $_REQUEST['untrashed'] ) );
 			unset($_REQUEST['undeleted']);
 		}
 		?>
@@ -426,7 +429,7 @@ class WPSC_Product_Variation_List_Table extends WP_List_Table {
 		$actions = array();
 
 		if ( $this->is_trash )
-			$actions['untrash'] = __( 'Restore' );
+			$actions['untrash'] = __( 'Restore', 'wp-e-commerce' );
 
 		if ( $this->is_draft )
 			$actions['show'] = __( 'Publish', 'wp-e-commerce' );
@@ -436,9 +439,9 @@ class WPSC_Product_Variation_List_Table extends WP_List_Table {
 		$actions['edit'] = __( 'Edit', 'wp-e-commerce' );
 
 		if ( $this->is_trash || !EMPTY_TRASH_DAYS )
-			$actions['delete'] = __( 'Delete Permanently' );
+			$actions['delete'] = __( 'Delete Permanently', 'wp-e-commerce' );
 		else
-			$actions['trash'] = __( 'Move to Trash' );
+			$actions['trash'] = __( 'Move to Trash', 'wp-e-commerce' );
 
 		return $actions;
 	}
@@ -461,7 +464,7 @@ class WPSC_Product_Variation_List_Table extends WP_List_Table {
 
 		echo '<input type="hidden" name="bulk_action_nonce" value="' . wp_create_nonce( 'wpsc_product_variations_bulk_action' ) .'" />';
 		echo "<select name='bulk_action$two'>\n";
-		echo "<option value='-1' selected='selected'>" . __( 'Bulk Actions' ) . "</option>\n";
+		echo "<option value='-1' selected='selected'>" . __( 'Bulk Actions', 'wp-e-commerce' ) . "</option>\n";
 
 		foreach ( $this->_actions as $name => $title ) {
 			$class = 'edit' == $name ? ' class="hide-if-no-js"' : '';
@@ -471,7 +474,7 @@ class WPSC_Product_Variation_List_Table extends WP_List_Table {
 
 		echo "</select>\n";
 
-		submit_button( __( 'Apply' ), 'action', false, false, array( 'id' => "doaction$two" ) );
+		submit_button( __( 'Apply', 'wp-e-commerce' ), 'action', false, false, array( 'id' => "doaction$two" ) );
 		echo "\n";
 	}
 
@@ -540,7 +543,7 @@ class WPSC_Product_Variation_List_Table extends WP_List_Table {
 		}
 
 		$class = empty( $class ) && empty( $_REQUEST['post_status'] ) && empty( $_REQUEST['show_sticky'] ) ? ' class="current"' : '';
-		$status_links['all'] = "<a href='{$url_base}'$class>" . sprintf( _nx( 'All <span class="count">(%s)</span>', 'All <span class="count">(%s)</span>', $total_posts, 'posts' ), number_format_i18n( $total_posts ) ) . '</a>';
+		$status_links['all'] = "<a href='{$url_base}'$class>" . sprintf( _nx( 'All <span class="count">(%s)</span>', 'All <span class="count">(%s)</span>', $total_posts, 'posts', 'wp-e-commerce' ), number_format_i18n( $total_posts ) ) . '</a>';
 
 		foreach ( get_post_stati(array('show_in_admin_status_list' => true), 'objects') as $status ) {
 			$class = '';
@@ -668,7 +671,7 @@ class WPSC_Product_Variation_List_Table extends WP_List_Table {
 		$post_type_object = get_post_type_object( 'wpsc-product' );
 		?><div class="alignleft actions"><?php
 		if ( $this->is_trash && current_user_can( $post_type_object->cap->edit_others_posts ) ) {
-			submit_button( __( 'Empty Trash' ), 'button-secondary apply', 'delete_all', false );
+			submit_button( __( 'Empty Trash', 'wp-e-commerce' ), 'button-secondary apply', 'delete_all', false );
 		}
 		?></div><?php
 	}

wpsc-admin/includes/product-variations-page.class.php
@@ -105,7 +105,7 @@ class WPSC_Product_Variations_Page {
 		check_admin_referer( 'wpsc_save_variations_meta', '_wpsc_save_meta_nonce' );
 		$post_type_object = get_post_type_object( 'wpsc-product' );
 		if ( ! current_user_can( $post_type_object->cap->edit_posts ) )
-			wp_die( __( 'Cheatin&#8217; uh?' ) );
+			wp_die( __( 'Cheatin&#8217; uh?', 'wp-e-commerce' ) );
 
 		/* Long-term, we should have a better saving routine here.  Can't unset these currently. *
 		/* That said, the only thing that fails hard if we can't unset it is the checkbox. */
@@ -210,10 +210,10 @@ class WPSC_Product_Variations_Page {
 		$trashed = 0;
 		foreach( (array) $post_ids as $post_id ) {
 			if ( !current_user_can( $post_type_object->cap->delete_post, $post_id ) )
-				wp_die( __( 'You are not allowed to move this item to the Trash.' ) );
+				wp_die( __( 'You are not allowed to move this item to the Trash.', 'wp-e-commerce' ) );
 
 			if ( !wp_trash_post( $post_id ) )
-				wp_die( __( 'Error in moving to Trash.' ) );
+				wp_die( __( 'Error in moving to Trash.', 'wp-e-commerce' ) );
 
 			$trashed++;
 		}
@@ -225,10 +225,10 @@ class WPSC_Product_Variations_Page {
 		$untrashed = 0;
 		foreach( (array) $post_ids as $post_id ) {
 			if ( ! current_user_can( $post_type_object->cap->delete_post, $post_id ) )
-				wp_die( __( 'You are not allowed to restore this item from the Trash.' ) );
+				wp_die( __( 'You are not allowed to restore this item from the Trash.', 'wp-e-commerce' ) );
 
 			if ( !wp_untrash_post( $post_id ) )
-				wp_die( __( 'Error in restoring from Trash.' ) );
+				wp_die( __( 'Error in restoring from Trash.', 'wp-e-commerce' ) );
 
 			$untrashed++;
 		}
@@ -242,14 +242,14 @@ class WPSC_Product_Variations_Page {
 			$post_del = & get_post( $post_id );
 
 			if ( ! current_user_can( $post_type_object->cap->delete_post, $post_id ) )
-				wp_die( __( 'You are not allowed to delete this item.' ) );
+				wp_die( __( 'You are not allowed to delete this item.', 'wp-e-commerce' ) );
 
 			if ( $post_del->post_type == 'attachment' ) {
 				if ( ! wp_delete_attachment( $post_id ) )
-					wp_die( __( 'Error in deleting...' ) );
+					wp_die( __( 'Error in deleting...', 'wp-e-commerce' ) );
 			} else {
 				if ( ! wp_delete_post( $post_id ) )
-					wp_die( __( 'Error in deleting...' ) );
+					wp_die( __( 'Error in deleting...', 'wp-e-commerce' ) );
 			}
 			$deleted++;
 		}

konstiantynr/wordpress-ecommerce

commit 3e09579657d8ccf63d2cc095c260213906a48607

Fix target market display, broken in d9864da668ad19302a4f86fdc8dbe693b299a4e8

wpsc-admin/includes/purchase-logs-page/item-details.php
@@ -89,7 +89,9 @@
 			<table class="widefat" cellspacing="0">
 				<thead>
 				<tr>
-					<?php print_column_headers( 'wpsc_purchase_log_item_details' ); ?>
+					<?php
+						print_column_headers( 'wpsc_purchase_log_item_details' );
+					 ?>
 				</tr>
 				</thead>
 

wpsc-admin/includes/save-data.functions.php
@@ -206,10 +206,10 @@ function wpsc_admin_category_forms_add() {
 					<?php foreach( $countrylist as $country ): ?>
 						<?php if ( in_array( $country['id'], (array)$selectedCountries ) ): ?>
 							<input type='checkbox' name='countrylist2[]' id='countrylist2-<?php echo $country['id']; ?>' value='<?php echo $country['id']; ?>' checked='<?php echo $country['visible']; ?>' />
-							<label for="countrylist2-<?php echo $country['id']; ?>"><?php esc_html( $country['country'] ); ?></label><br />
+							<label for="countrylist2-<?php echo $country['id']; ?>"><?php echo esc_html( $country['country'] ); ?></label><br />
 						<?php else: ?>
 							<input type='checkbox' name='countrylist2[]' id='countrylist2-<?php echo $country['id']; ?>' value='<?php echo $country['id']; ?>'  />
-							<label for="countrylist2-<?php echo $country['id']; ?>"><?php esc_html( $country['country'] ); ?></label><br />
+							<label for="countrylist2-<?php echo $country['id']; ?>"><?php echo esc_html( $country['country'] ); ?></label><br />
 						<?php endif; ?>
 					<?php endforeach; ?>
 				</div>
@@ -401,12 +401,12 @@ function wpsc_admin_category_forms_edit() {
 						if ( in_array( $country['id'], (array)$selectedCountries ) ) {
 							?>
 							<input type='checkbox' name='countrylist2[]' id='countrylist2-<?php echo $country['id']; ?>' value='<?php echo $country['id']; ?>' checked='<?php echo $country['visible']; ?>' />
-							<label for="countrylist2-<?php echo $country['id']; ?>"><?php esc_html( $country['country'] ); ?></label><br />
+							<label for="countrylist2-<?php echo $country['id']; ?>"><?php echo esc_html( $country['country'] ); ?></label><br />
 							<?php
 						} else {
 							?>
 							<input type='checkbox' name='countrylist2[]' id='countrylist2-<?php echo $country['id']; ?>' value='<?php echo $country['id']; ?>'  />
-							<label for="countrylist2-<?php echo $country['id']; ?>"><?php esc_html( $country['country'] ); ?></label><br />
+							<label for="countrylist2-<?php echo $country['id']; ?>"><?php echo esc_html( $country['country'] ); ?></label><br />
 							<?php
 						}
 					} ?>

wpsc-admin/includes/settings-tabs/general.php
@@ -95,10 +95,10 @@ class WPSC_Settings_Tab_General extends WPSC_Settings_Tab {
 								<?php foreach ( (array)$countrylist as $country ) : ?>
 									<?php if ( $country['visible'] == 1 ) : ?>
 										<input type='checkbox' id="countrylist2-<?php echo $country['id']; ?>" name='countrylist2[]' value='<?php echo $country['id']; ?>' checked='checked' />
-										<label for="countrylist2-<?php echo $country['id']; ?>"><?php esc_html( $country['country'] ); ?></label><br />
+										<label for="countrylist2-<?php echo $country['id']; ?>"><?php echo esc_html( $country['country'] ); ?></label><br />
 									<?php else : ?>
 										<input type='checkbox' id="countrylist2-<?php echo $country['id']; ?>" name='countrylist2[]' value='<?php echo $country['id']; ?>'  />
-										<label for="countrylist2-<?php echo $country['id']; ?>"><?php esc_html( $country['country'] ); ?></label><br />
+										<label for="countrylist2-<?php echo $country['id']; ?>"><?php echo esc_html( $country['country'] ); ?></label><br />
 									<?php endif; ?>
 								<?php endforeach; ?>
 							</div>
@@ -155,7 +155,7 @@ class WPSC_Settings_Tab_General extends WPSC_Settings_Tab {
 				<td>
 					<select id="wpsc_options_currency_type" name='wpsc_options[currency_type]' onchange='getcurrency(this.options[this.selectedIndex].value);'>
 					<?php foreach ( $currency_data as $currency ) : ?>
-						<option value='<?php echo $currency['id']; ?>' <?php selected( $currency['id'], $currency_type ); ?>><?php esc_html( $currency['country'] ); ?> (<?php echo $currency['currency']; ?>)</option>
+						<option value='<?php echo $currency['id']; ?>' <?php selected( $currency['id'], $currency_type ); ?>><?php echo esc_html( $currency['country'] ); ?> (<?php echo $currency['currency']; ?>)</option>
 					<?php endforeach; ?>
 					</select>
 				</td>